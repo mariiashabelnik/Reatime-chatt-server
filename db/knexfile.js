8b19e79631ed52c9d6f68ea3e4de918a43e78bb2
Mariia Shabelnik <>
2022-08-24 13:20:18 +0200

    Add postgres

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,11 +3,23 @@ const path = require("path");
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+
+/*const config = {};
+
+config.sqlLite = {
   client: "sqlite3",
   connection: {
     filename: path.join(__dirname, "db.sqlite3"),
   },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+  useNullAsDefault: true,
+}; */
+
+module.exports = {
+  client: "pg",
+  connection: process.env.DATABASE_URL,
   migrations: {
     tableName: "knex_migrations",
   },
